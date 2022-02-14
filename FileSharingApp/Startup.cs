2d6cf8a79725c8b48f9a3391d726e5023d5fb8f9
dotnetdeveloper5 <melbetar58@gmail.com>
2022-02-14 22:52:52 +0200

    User Service and Dashboard

--- FileSharingApp/Startup.cs
+++ FileSharingApp/Startup.cs
@@ -95,6 +95,10 @@ namespace FileSharingApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
